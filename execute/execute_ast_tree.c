c97ec05151521b8c0b916f721789d2b6dadff23d
Marsha Teo <mateo@lab3r7s7.42abudhabi.ae>
2024-05-27 12:14:16 +0400

    draft idea of how execution will work

--- execute/execute_ast_tree.c
+++ execute/execute_ast_tree.c
@@ -6,23 +6,43 @@
 /*   By: mateo <mateo@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 18:04:03 by mateo             #+#    #+#             */
-/*   Updated: 2024/05/25 18:42:56 by mateo            ###   ########.fr       */
+/*   Updated: 2024/05/27 12:13:44 by mateo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/*	execute_ast executes AST*/
+/*	execute_cmd checks whether command is built in and runs it if so
+	otherwise, it searches for binary file for command */
+int	execute_cmd(t_ast *node, int in_fd, int out_fd)
+{
+	if (check_builtin(node->cmd) == 0)
+		run_builtin(node, in_fd, out_fd);
+	else
+		run_external(node, in_fd, out_fd);
+}
+
+/*	execute_cmd_node
+	- sets up file descriptors given redirections
+	- executes command with updated input/output fd */
+int	execute_cmd_node(t_ast *node)
+{
+	// int	exit_status;
+	// handle redirections
+	// exit_status = execute_cmd(node, );
+	// return (exit_status);
+}
+
+/*	execute_ast traverses AST for execution
+	- includes logic for TOKEN_AND and TOKEN_OR (which recursively calls execute_ast)
+	*/
 int	execute_ast(t_ast *node)
 {
 	if (!node)
 		return (0);
 	if (!node->left && !node->right) 
 	{
-		// execute command - return 1 or 0
-		// return (execute_command(node));
-
-		printf("exe cmd: %s\n", node->cmd);
+		return (execute_cmd_node(node));
 		return (0);
 	}
 	else if (node->code == TOKEN_PIPE)
