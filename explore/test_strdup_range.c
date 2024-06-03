9c87392c3af99aec692121724daf4604ef5a0733
Marsha <151203981+marsha-t@users.noreply.github.com>
2024-06-03 13:01:14 +0000

    fix strdup_range

--- explore/test_strdup_range.c
+++ explore/test_strdup_range.c
@@ -7,8 +7,9 @@ char *strdup_range(char *start, char *end)
 	size_t	n;
 	int i;
 
-	n = end - start;
+	n = end - start + 1;
 	ptr = malloc(sizeof(char) * (n + 1));
+	printf("n + 1: %ld\n", n + 1);
 	if (!ptr)
 		return (0);
 	i = 0;
@@ -16,10 +17,13 @@ char *strdup_range(char *start, char *end)
 	{
 		ptr[i] = *start;
 		start++;
+		printf("%c\n", ptr[i]);
 		i++;
 	}
 	ptr[i] = *end;
+	printf("%c\n", ptr[i]);
 	ptr[i+1] = '\0';
+	printf("i+1: %d\n", i+1);
 	return (ptr);
 }
 
@@ -34,21 +38,24 @@ int ft_strlen(char *str)
 }
 int main()
 {
-	char text[4];
+	char text[7];
 	text[0] = 'a';
 	text[1] = 'b';
 	text[2] = 'c';
-	text[3] = '\0';
+	text[3] = 'd';
+	text[4] = 'e';
+	text[5] = 'f';
+	text[6] = '\0';
 
 	char *start;
 	char *end;
 	start = text;
-	end = start + 1;
+	end = start + 3;
 
 	printf("%c, %c\n", *start, *end);
 	printf("%ld\n", end - start);
 	printf("%s\n", strdup_range(start, end));
-	printf("%d\n", ft_strlen(strdup_range(start, end)));
-	if (strdup_range(start, end)[2] == '\0')
-		printf("nullterm\n");
+	// printf("%d\n", ft_strlen(strdup_range(start, end)));
+	// if (strdup_range(start, end)[2] == '\0')
+	// 	printf("nullterm\n");
 }
