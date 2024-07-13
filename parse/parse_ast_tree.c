851bfa4c0cd682037821bdf401c5c427a470ab5b
Marsha <151203981+marsha-t@users.noreply.github.com>
2024-07-13 05:09:21 +0000

    fix parsing issue with brackets

--- parse/parse_ast_tree.c
+++ parse/parse_ast_tree.c
@@ -77,6 +77,10 @@ t_ast	*ast_tree_new(t_ast **node)
 	{
 		*node = (*node)->next;
 		root = ast_tree_new(node);
+		if ((*node) && (*node)->code == TOKEN_PIPE)
+		{
+			root->pipe = ast_pipeline_new(node);			
+		}	
 	}
 	while (*node)
 	{
